4f03fadce1bcd7982c4dd1f73bb4211ad376707d
Salman <salman39302@gmail.com>
2025-12-30 12:00:29 +0530

    Added async Handler for neat code

---
+++ Backend/Src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+
+// Utility to handle async errors in Express routes
+
+const asyncHandler = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+module.exports = asyncHandler;

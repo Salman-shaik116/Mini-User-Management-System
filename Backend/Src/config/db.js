7418dd18ed6fd3eb235220dbeca3ec873485c425
Salman <salman39302@gmail.com>
2025-12-30 11:47:43 +0530

    The DataBase Configuration

---
+++ Backend/Src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+
+// Function to connect to MongoDB using Mongoose and handle connection errors through try-catch
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;

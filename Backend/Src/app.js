a1b4d008e67974d3c95b05e1e99c68f7de8be6ef
Salman Shaik <154886467+Salman-shaik116@users.noreply.github.com>
2025-12-31 20:38:47 +0530

    modified cors configuration for prod deployment

--- Backend/Src/app.js
+++ Backend/Src/app.js
@@ -19,7 +19,7 @@ app.use(
             const configured = process.env.CLIENT_ORIGIN;
             const allowedOrigins = (configured
                 ? configured.split(",")
-                : ["http://localhost:5173", "http://localhost:5174"]
+                : ["http://localhost:5173", "http://localhost:5174","https://mini-user-management-system-git-main-salmans-projects-c8054218.vercel.app"]
             ).map((value) => value.trim());
 
             if (allowedOrigins.includes(origin)) {
